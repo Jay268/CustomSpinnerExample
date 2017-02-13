309cdcc6ffe2a88ec43e5241348003600f160a5e
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-02-14 01:28:12 +0200

    Add simple validation.

--- app/src/main/java/com/ysered/customspinnerexample/MainActivity.java
+++ app/src/main/java/com/ysered/customspinnerexample/MainActivity.java
@@ -2,20 +2,49 @@ package com.ysered.customspinnerexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.ysered.customspinnerexample.view.CompoundSpinner;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
-    private Spinner spinner;
+    private CompoundSpinner spinner;
+    private TextView errorsText;
+    private Button validateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner = (CompoundSpinner) findViewById(R.id.compoundSpinner);
+        errorsText = (TextView) findViewById(R.id.errorsText);
+        validateButton = (Button) findViewById(R.id.validateButton);
+
         final String[] items = getResources().getStringArray(R.array.spinner_items);
         final SpinnerAdapter adapter = new SpinnerAdapter(this, items);
         spinner.setAdapter(adapter);
+
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String text = spinner.getSelectedItemText();
+                Log.d(TAG, "Selected text: " + text);
+                if (text == null) {
+                    errorsText.setVisibility(View.VISIBLE);
+                    errorsText.setText(getString(R.string.error_select_item));
+                } else if (text.isEmpty()) {
+                    errorsText.setVisibility(View.VISIBLE);
+                    errorsText.setText(getString(R.string.error_enter_text));
+                } else {
+                    errorsText.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
