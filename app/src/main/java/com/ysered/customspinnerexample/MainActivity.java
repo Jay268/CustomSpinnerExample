545e11614d5eeb274096a2f46fdc5816b0c3cce0
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-02-13 22:59:04 +0200

    Instantiate spinner. Set adapter.

--- app/src/main/java/com/ysered/customspinnerexample/MainActivity.java
+++ app/src/main/java/com/ysered/customspinnerexample/MainActivity.java
@@ -2,12 +2,22 @@ package com.ysered.customspinnerexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        final String[] items = getResources().getStringArray(R.array.spinner_items);
+        spinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items));
     }
 }
