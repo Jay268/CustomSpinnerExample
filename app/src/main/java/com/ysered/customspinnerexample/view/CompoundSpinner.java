226a9ebf88a0f77042c4997ed9cfb4618418f70a
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-02-14 01:13:19 +0200

    Get rid of extra view class.

--- app/src/main/java/com/ysered/customspinnerexample/view/CompoundSpinner.java
+++ app/src/main/java/com/ysered/customspinnerexample/view/CompoundSpinner.java
@@ -2,6 +2,8 @@ package com.ysered.customspinnerexample.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -14,10 +16,12 @@ import com.ysered.customspinnerexample.R;
 public class CompoundSpinner extends FrameLayout {
     private static final String TAG = CompoundSpinner.class.getSimpleName();
 
+    private static final int DRAWABLE_RIGHT = 2;
+
     private boolean isSelected = false;
     private TextView placeholderText;
     private Spinner spinner;
-    private SpinnerEditText spinnerEditText;
+    private EditText customText;
 
     public CompoundSpinner(Context context) {
         super(context);
@@ -37,8 +41,8 @@ public class CompoundSpinner extends FrameLayout {
     public String getSelectedItemText() {
         String text = null;
         final boolean isPlaceholderShown = placeholderText.getVisibility() != VISIBLE;
-        if (!isPlaceholderShown && spinnerEditText.getVisibility() == VISIBLE) {
-            text = spinnerEditText.getText().toString();
+        if (!isPlaceholderShown && customText.getVisibility() == VISIBLE) {
+            text = customText.getText().toString();
         } else if (!isPlaceholderShown && spinner.getVisibility() == VISIBLE) {
             text = spinner.getSelectedItem().toString();
         }
@@ -48,6 +52,10 @@ public class CompoundSpinner extends FrameLayout {
     private void init() {
         final View view = inflate(getContext(), R.layout.view_compound_spinner, this);
         placeholderText = (TextView) view.findViewById(R.id.placeholderText);
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        customText = (EditText) view.findViewById(R.id.customText);
+
+        // hide placeholder text and show spinner items
         placeholderText.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,15 +64,7 @@ public class CompoundSpinner extends FrameLayout {
                 spinner.performClick();
             }
         });
-        spinner = (Spinner) view.findViewById(R.id.spinner);
-        spinnerEditText = (SpinnerEditText) view.findViewById(R.id.spinnerEditText);
-        spinnerEditText.setOnClickListener(new SpinnerEditText.OnClickListener() {
-            @Override
-            public void onClick(EditText editText) {
-                editText.setVisibility(INVISIBLE);
-                spinner.performClick();
-            }
-        });
+        // show EditText if last item selected or regular item otherwise
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -73,8 +73,9 @@ public class CompoundSpinner extends FrameLayout {
                     spinner.setVisibility(INVISIBLE);
                 } else {
                     isSelected = true;
+                    customText.setVisibility(INVISIBLE);
                     if (position == parent.getCount() - 1) {
-                        spinnerEditText.setVisibility(VISIBLE);
+                        customText.setVisibility(VISIBLE);
                     }
                 }
             }
@@ -82,6 +83,20 @@ public class CompoundSpinner extends FrameLayout {
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+        // show spinner items when on right image of EditText clicked
+        customText.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (getRight() - customText.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        Log.d(TAG, "onTouch: touched on drawable");
+                        spinner.performClick();
+                        return true;
+                    }
+                }
+                return false;
             }
         });
     }
